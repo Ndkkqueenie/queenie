0059f209141784318475fd460fff56c4319e2817
Queendalin Ezenwa <queendalin.nduka@gmail.com>
2023-11-25 16:09:25 +0100

    update

---
+++ js/myfunction.js
@@ -0,0 +1,92 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "➕"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "✖️"; 
+    moreText.style.display = "inline";
+  }
+}
+function eStoreReadMore() {
+  var dots = document.getElementById("eStoreDots");
+  var moreText = document.getElementById("eStoreMore");
+  var btnText = document.getElementById("eStoremyBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "➕"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "✖️"; 
+    moreText.style.display = "inline";
+  }
+}
+function ristoranteReadMore() {
+  var dots = document.getElementById("ristoranteDots");
+  var moreText = document.getElementById("ristoranteMore");
+  var btnText = document.getElementById("ristorantemyBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "➕"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "✖️"; 
+    moreText.style.display = "inline";
+  }
+}
+function babysitReadMore() {
+  var dots = document.getElementById("babysitDots");
+  var moreText = document.getElementById("babysitMore");
+  var btnText = document.getElementById("babysitmyBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "➕"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "✖️"; 
+    moreText.style.display = "inline";
+  }
+}
+function spendReadMore() {
+  var dots = document.getElementById("spendDots");
+  var moreText = document.getElementById("spendMore");
+  var btnText = document.getElementById("spendmyBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "➕"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "✖️"; 
+    moreText.style.display = "inline";
+  }
+}
+function expoReadMore() {
+  var dots = document.getElementById("expoDots");
+  var moreText = document.getElementById("expoMore");
+  var btnText = document.getElementById("expomyBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "➕"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "✖️"; 
+    moreText.style.display = "inline";
+  }
+}
